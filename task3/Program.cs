64c23ab31db7671cfecf383e2d594d80d1c77d3f
Tatiana <kravchenkodevelop@gmail.com>
2022-10-07 17:23:58 +0300

    task3 complete

---
+++ task3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine() ?? "0");
+if (a % 2 == 0) { Console.WriteLine($"Число {a} чётное"); }
+else { Console.WriteLine($"Число {a} нечётное"); }
+
+
+Console.ReadKey();
