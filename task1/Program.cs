d198d59ac11f97714f7958d9aa526495a275ba41
Tatiana <kravchenkodevelop@gmail.com>
2022-10-07 16:42:44 +0300

    task1 complete

---
+++ task1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите первое число");
+int a =int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите второе число");
+int b =int.Parse(Console.ReadLine() ?? "0");
+if  (a == b) {Console.WriteLine("Числа равны");}
+else if (a<b) { Console.WriteLine($"Число {a} меньшее, число {b} большее"); }
+else { Console.WriteLine($"Число {b} меньшее, число {a} большее");}
+
+Console.ReadKey();
