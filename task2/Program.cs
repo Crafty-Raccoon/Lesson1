ebfec10282b40c94689f0963db4f77460ff5ace7
Tatiana <kravchenkodevelop@gmail.com>
2022-10-07 17:13:45 +0300

    task2 complete

---
+++ task2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите три числа");
+int a = int.Parse (Console.ReadLine()??"0");
+int b = int.Parse (Console.ReadLine()??"0");
+int c = int.Parse (Console.ReadLine()??"0");
+int[] arr = new int[] { a, b, c };
+int max = arr.Max();
+
+Console.WriteLine(max);
+
+Console.ReadKey();
