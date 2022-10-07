f0bff7a17839b6fb44ece526c6416ef3c28cffda
Tatiana <kravchenkodevelop@gmail.com>
2022-10-07 17:45:20 +0300

    task4 complete

---
+++ task4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine() ?? "0");
+if (a == 1) { Console.WriteLine("Введите другое число"); }
+else
+{
+    if (a > 0)
+    {
+        for (int i = 1; i <= a; i++)
+        {
+            if (i % 2 == 0) { Console.WriteLine(i); }
+        }
+    }
+    else
+    {
+        for (int i = 1; i >= a; i--)
+        {
+            if (i % 2 == 0) { Console.WriteLine(i); }
+        }
+    }
+
+}
+
+Console.ReadKey();
